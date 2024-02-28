c421ad2f79411a5e3b23c3e109b51daa4cd78e7f
jeffmikhail <mikhailcruz78@gmail.com>
2024-02-28 20:34:06 +0800

    refactor

--- js/arithmetic.test.js
+++ js/arithmetic.test.js
@@ -1,21 +1,25 @@
 const { add, subtract, multiply, divide } = require("./arithmetic");
 
 // *** add
-test("properly add two numbers", () => {
-	expect(add(10, 5)).toBe(15);
+it("should add two numbers", () => {
+	const result = add(10, 5);
+	expect(result).toBe(15);
 });
 
 // *** subtract
-test("properly subtract two numbers", () => {
-	expect(subtract(10, 5)).toBe(5);
+it("should subtract two numbers", () => {
+	const result = subtract(10, 5);
+	expect(result).toBe(5);
 });
 
 // *** multiply
-test("properly multiply two numbers", () => {
-	expect(multiply(10, 5)).toBe(50);
+it("should multiply two numbers", () => {
+	const result = multiply(10, 5);
+	expect(result).toBe(50);
 });
 
 // *** divide
-test("properly divide two numbers", () => {
-	expect(divide(10, 5)).toBe(2);
+it("should divide two numbers", () => {
+	const result = divide(10, 5);
+	expect(result).toBe(2);
 });
